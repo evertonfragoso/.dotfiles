3d5a3512541617254b61efbb1a85e4e6435ad394
Everton Fragoso <everton.lfragoso@gmail.com>
2018-08-20 14:15:46 +1200

    changes

--- brew_formulas.sh
+++ brew_formulas.sh
@@ -8,9 +8,9 @@ installed_formulae=$(brew list)
 while IFS='' read -r line || [[ -n "$line" ]]; do
   if [[ installed_formulae == *"${line}"* ]]; then
     echo "Installing ${line}"
-    brew install ${line}
+    brew install $line
   else
     echo -e "\033[0;46m[INFO]\033[0m ${line} already installed. Updating..."
-    brew upgrade ${line} --cleanup
+    brew upgrade $line --cleanup
   fi
 done < "$brew_list"
