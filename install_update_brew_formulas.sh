9948f4965137ef6adc79ec7a7eb00b256acd4fba
Everton Fragoso <everton.lfragoso@gmail.com>
2019-01-22 11:01:08 +1300

    Update deprecated brew flag

--- install_update_brew_formulas.sh
+++ install_update_brew_formulas.sh
@@ -11,6 +11,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     brew install $line
   else
     echo -e "\033[0;46m[INFO]\033[0m ${line} already installed. Updating..."
-    brew upgrade $line --cleanup
+    export HOMEBREW_INSTALL_CLEANUP="1"
+    brew upgrade $line
   fi
 done < "$brew_list"
