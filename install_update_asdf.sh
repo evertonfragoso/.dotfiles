39bfaf887d9a866fec3cc90a2dfca6234481a496
Everton Fragoso <everton.lfragoso@gmail.com>
2019-02-22 12:35:17 +1300

    asdf changes

---
+++ install_update_asdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add vars
+. ./setup_vars.sh
+
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  asdf install ruby ${RUBY_VERSION}
+  asdf install node ${NODE_VERSION}
+else
+  asdf update
+  asdf plugin-update --all
+fi
+
+cd $SETUP_FOLDER
--- setup_vars.sh
+++ setup_vars.sh
@@ -2,6 +2,8 @@
 
 # TODO: make it get the latest version instead of a fixed version
 export RUBY_VERSION=2.5.3
+export NODE_VERSION=10.13.0
+
 export SETUP_FOLDER=${PWD}
 export VIM_FOLDER="$SETUP_FOLDER/vim"
 export bundle_folder="$VIM_FOLDER/bundle"
