057944bdbf716bb6066695c8d3dab8f1fe840caa
Everton Fragoso <everton.lfragoso@gmail.com>
2020-02-10 11:11:19 +1300

    changes

--- install_update_brew_formulas.sh
+++ install_update_brew_formulas.sh
@@ -5,13 +5,18 @@ brew tap universal-ctags/universal-ctags
 
 brew_list='brew_list.txt'
 installed_formulae=$(brew list)
+
+# Install formulas from list
 while IFS='' read -r line || [[ -n "$line" ]]; do
-  if [[ installed_formulae == *"${line}"* ]]; then
+  if [[ $installed_formulae != *"${line}"* ]]; then
     echo "Installing ${line}"
     brew install $line
-  else
-    echo -e "\033[0;46m[INFO]\033[0m ${line} already installed. Updating..."
-    export HOMEBREW_INSTALL_CLEANUP="1"
-    brew upgrade $line
   fi
-done < "$brew_list"
+done < $brew_list
+
+# Upgrade all formulaes
+for formulae in $installed_formulae; do
+  echo -e "\033[0;46m[INFO]\033[0m ${line} already installed. Updating..."
+  export HOMEBREW_INSTALL_CLEANUP="1"
+  brew upgrade $formulae
+done
